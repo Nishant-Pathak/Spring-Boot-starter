7d1cfaaf3b2596a9cc25f55b79332b82e5b6b7e9
nishant <nishantpathak.cse@gmail.com>
2017-02-09 10:36:39 +0530

    initial commit

--- src/main/java/rs/pscode/pomodorofire/Main.java
+++ src/main/java/rs/pscode/pomodorofire/Main.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
 	public static void main(String[] args) {
+		String [] argsWithDebugFlag = new String[args.length + 1];
+		System.arraycopy(args, 0, argsWithDebugFlag, 0, args.length);
+		argsWithDebugFlag[args.length] = "--debug";
 		SpringApplication application = new SpringApplication(Main.class);
-		application.run(new String[] { "--debug" });
+		application.run(argsWithDebugFlag);
 	}
 }
